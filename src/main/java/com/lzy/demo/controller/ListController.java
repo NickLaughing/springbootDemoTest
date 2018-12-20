4679a760267e1ea427c5856c51c9f33fd6642edc
DESKTOP-HIO8IJT\NickLzy <liangzhiyi8854@sina.com>
2018-12-20 15:40:41 +0800

    getPerson

--- src/main/java/com/lzy/demo/controller/ListController.java
+++ src/main/java/com/lzy/demo/controller/ListController.java
@@ -1,8 +1,17 @@
 package com.lzy.demo.controller;
 
+import com.lzy.demo.pojo.Person;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController(ListController.DOMAIN)
 public class ListController {
     public static final String DOMAIN = "/list";
+
+    @GetMapping()
+    public List<Person> getPerson() {
+        return null;
+    }
 }
